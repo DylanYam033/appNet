213698d34decda305876c3befb1aad3325afaf23
DylanYam033 <dylan.yampuezan@wactechnology.com>
2024-06-12 08:04:30 -0500

    add loading skeletons

--- crudapp.client/src/App.jsx
+++ crudapp.client/src/App.jsx
@@ -5,26 +5,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ModalCreate } from './components/ModalCreate';
 import Swal from 'sweetalert2';
 import 'react-toastify/dist/ReactToastify.css';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function App() {
 
     const [contactos, setContactos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const mostrarContactos = async () => {
-        const response = await fetch('api/Contacto/contactos/list');
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            setContactos(data);
-        } else {
-            console.error("Error")
+        setLoading(true);
+        try {
+            const response = await fetch('api/Contacto/contactos/list');
+            if (response.ok) {
+                const data = await response.json();
+                setContactos(data);
+            } else {
+                console.error("Error al cargar los contactos");
+            }
+        } catch (error) {
+            console.error("Error de red:", error);
         }
+        setLoading(false); // ponemos en false el loading despues de cargar los contactos
     }
 
     useEffect(() => {
-        mostrarContactos()
-    }, [])
+        mostrarContactos();
+    }, []);
 
     const [showModal, setShowModal] = useState(false);
 
@@ -34,41 +41,36 @@ function App() {
 
     const handleSaveContact = async (contact) => {
         try {
-          const response = await fetch('/api/Contacto/contactos/create', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(contact),
-          });
-    
-          if (response.ok) {
-            // Mostrar alerta de éxito
-            Swal.fire({
-              icon: 'success',
-              title: '¡Éxito!',
-              text: 'Contacto guardado exitosamente',
+            const response = await fetch('/api/Contacto/contactos/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(contact),
             });
-            toggleModal(); // Cerrar modal después de guardar exitosamente
-            mostrarContactos();
-          } else {
-            // Mostrar alerta de error
-            Swal.fire({
-              icon: 'error',
-              title: 'Error',
-              text: 'Hubo un problema al guardar el contacto. Por favor, inténtalo de nuevo más tarde.',
-            });
-            
-          }
+
+            if (response.ok) {
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Éxito!',
+                    text: 'Contacto guardado exitosamente',
+                });
+                toggleModal();
+                mostrarContactos();
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Hubo un problema al guardar el contacto. Por favor, inténtalo de nuevo más tarde.',
+                });
+            }
         } catch (error) {
-          console.error('Error de red:', error);
-          // Mostrar alerta de error de red
-          Swal.fire({
-            icon: 'error',
-            title: 'Error de red',
-            text: 'Hubo un problema al conectarse al servidor. Por favor, revisa tu conexión a internet e inténtalo de nuevo.',
-          });
-         
+            console.error('Error de red:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error de red',
+                text: 'Hubo un problema al conectarse al servidor. Por favor, revisa tu conexión a internet e inténtalo de nuevo.',
+            });
         }
     };
 
@@ -83,7 +85,11 @@ function App() {
                         <CardBody>
                             <Button color="success" onClick={toggleModal}>Nuevo contacto</Button>
                             <hr></hr>
-                            <TablaContacto contactos={contactos} mostrarContactos={mostrarContactos} />
+                            {loading ? (
+                                <Skeleton count={5} height={40} />
+                            ) : (
+                                <TablaContacto contactos={contactos} mostrarContactos={mostrarContactos} />
+                            )}
                         </CardBody>
                     </Card>
                 </Col>
@@ -91,7 +97,6 @@ function App() {
             <ModalCreate isOpen={showModal} toggle={toggleModal} onSave={handleSaveContact} />
         </Container>
     );
-
 }
 
 export default App;
